cb97fbd02f4cbff8770970c17556489a8b37f6db
buttfa <1662332017@qq.com>
2024-08-03 14:23:46 +0800

    删除调用任务时的额外无用输出 / Remove additional useless output when calling tasks

--- src/instructions/task/task.c
+++ src/instructions/task/task.c
@@ -98,7 +98,7 @@ void isShellTask(section *ccc_section) {
         ccc_section->name = task_name;
 
         // 执行Shell任务
-        printf("[CCC]Shell task: %s\n", task_name);
+        // printf("[CCC]Shell task: %s\n", task_name);
         for (int i = 0; i < ccc_section->kvp_num; i++) {
             // 如果key为NULL或null开头，则不输出key
             if (strlen(ccc_section->kvps[i]->key)>=6 && (strncmp(ccc_section->kvps[i]->key, "(NULL)",6)==0||strncmp(ccc_section->kvps[i]->key, "(null)",6)==0)){
@@ -109,7 +109,7 @@ void isShellTask(section *ccc_section) {
             system(ccc_section->kvps[i]->value);
         }
 
-        printf("[CCC]Shell task: %s done\n", task_name);
+        // printf("[CCC]Shell task: %s done\n", task_name);
     }
 }
 
@@ -127,12 +127,12 @@ void isCompileTask(section *ccc_section) {
         ccc_section->name = task_name;
 
         // 执行编译任务
-        printf("[CCC]Compile task: %s\n", task_name);
+        // printf("[CCC]Compile task: %s\n", task_name);
         task = parseCCCIni(ccc_section);
         handTask(task);
 
         // 释放任务
         freeTask(&task);
-        printf("[CCC]Compile task: %s done\n", task_name);
+        // printf("[CCC]Compile task: %s done\n", task_name);
     }
 }
