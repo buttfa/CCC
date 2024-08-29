39924a73767d8115c8e5c7677afb393297627c51
buttfa <1662332017@qq.com>
2024-08-29 21:53:03 +0800

    style: 补充了file.c/.h的文件和函数注释 / Added file and function comments for file.c/.h

--- inc/instructions/task/file.h
+++ inc/instructions/task/file.h
@@ -1,5 +1,15 @@
 #ifndef __FILE_H__
 #define __FILE_H__
+/**
+ * @file file.h
+ * @author  buttfa (1662332017@qq.com)
+ * @brief 该头文件定义了字符串操作相关的函数和数据结构。
+ * @version 0.1
+ * @date 2024-08-29
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
 
 #include <stdio.h>
 #include <string.h>
@@ -30,34 +40,34 @@ struct header_folder {
  * @brief 接受一个文件路径path和一个后缀字符串suffix作为
  *        参数，判断该路径对应的文件是否存在并且文件名以指定的后缀结束。
  * 
- * @param path 
- * @param suffix 
- * @return true 
- * @return false 
+ * @param path 文件路径
+ * @param suffix 后缀字符串
+ * @return true 文件存在且文件名以指定的后缀结束
+ * @return false 文件不存在或不以指定的后缀结束
  */
 bool isFileWithSuffix(char *path, char *suffix);
 
 /**
  * @brief 将字符串str拆分成以delimiter为分隔符的数组，返回数组指针。
  * 
- * @param str 
- * @param delimiter 
- * @return char** 
+ * @param str 需要分割的字符串
+ * @param delimiter 分割符
+ * @return char** 分割后的字符串数组
  */
 char **splitString(char *str, char delimiter);
 
 /**
  * @brief 释放分割字符串的数组。
  * 
- * @param splitResult 
+ * @param splitResult 需要释放的数组指针
  */
 void freeSplitResult(char **splitResult);
 
 /**
  * @brief 获取到filePath去除路径和后缀的文件名
  * 
- * @param filePath 
- * @return char* 
+ * @param filePath 需要提取的文件名的文件路径
+ * @return char* 提取出的文件名
  */
 char* getFileName(char* filePath);
 
@@ -65,25 +75,25 @@ char* getFileName(char* filePath);
 /**
  * @brief 提取input_str中的子字符串，以空格为分隔符
  * 
- * @param input_str 
- * @param out_num_tokens 
- * @return char** 
+ * @param input_str 需要分割的字符串
+ * @param out_num_tokens 分割后产生的字符串个数
+ * @return char** 字符串数组
  */
 char** splitStringBySpace(char* input_str, int* out_num_tokens);
 
 /**
  * @brief 使用于本项目的特殊strlen，当str指向NULL时，返回0
  * 
- * @param str 
- * @return int 
+ * @param str 需要获取长度的字符串
+ * @return int 字符串长度
  */
 int hotfix_strlen(char* str);
 
 /**
  * @brief 使用于本项目的特殊strcat，当dest或str指向NULL时，不进行操作
  * 
- * @param dest 
- * @param src 
+ * @param dest 目标字符串
+ * @param src 源字符串
  */
 void hotfix_strcat(char* dest, char* src);
 #endif
--- src/instructions/task/file.c
+++ src/instructions/task/file.c
@@ -1,4 +1,24 @@
+/**
+ * @file file.c
+ * @author  buttfa (1662332017@qq.com)
+ * @brief 实现了所需的字符串操作的函数
+ * @version 0.1
+ * @date 2024-08-29
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
 #include <file.h>
+
+/**
+ * @brief 接受一个文件路径path和一个后缀字符串suffix作为
+ *        参数，判断该路径对应的文件是否存在并且文件名以指定的后缀结束。
+ * 
+ * @param path 文件路径
+ * @param suffix 后缀字符串
+ * @return true 文件存在且文件名以指定的后缀结束
+ * @return false 文件不存在或不以指定的后缀结束
+ */
 bool isFileWithSuffix(char *path, char *suffix) {
     // 首先检查路径是否以suffix结尾
     size_t path_len = hotfix_strlen(path);
@@ -25,9 +45,9 @@ bool isFileWithSuffix(char *path, char *suffix) {
 /**
  * @brief 将字符串str拆分成以delimiter为分隔符的数组，返回数组指针。
  * 
- * @param str 
- * @param delimiter 
- * @return char** 
+ * @param str 需要分割的字符串
+ * @param delimiter 分割符
+ * @return char** 分割后的字符串数组
  */
 char **splitString(char *str, char delimiter) {
     if (str == NULL) return NULL;
@@ -75,7 +95,7 @@ char **splitString(char *str, char delimiter) {
 /**
  * @brief 释放分割字符串的数组。
  * 
- * @param splitResult 
+ * @param splitResult 需要释放的数组指针
  */
 void freeSplitResult(char **splitResult) {
     if (splitResult != NULL) {
@@ -89,8 +109,8 @@ void freeSplitResult(char **splitResult) {
 /**
  * @brief 获取到filePath去除路径和后缀的文件名
  * 
- * @param filePath 
- * @return char* 
+ * @param filePath 需要提取的文件名的文件路径
+ * @return char* 提取出的文件名
  */
 char* getFileName(char* filePath) {
     if (filePath == NULL) return NULL;
@@ -125,9 +145,9 @@ char* getFileName(char* filePath) {
 /**
  * @brief 提取input_str中的子字符串，以空格为分隔符
  * 
- * @param input_str 
- * @param out_num_tokens 
- * @return char** 
+ * @param input_str 需要分割的字符串
+ * @param out_num_tokens 分割后产生的字符串个数
+ * @return char** 字符串数组
  */
 char** splitStringBySpace(char* input_str, int* out_num_tokens) {
     if (input_str != NULL) {
@@ -198,8 +218,8 @@ char** splitStringBySpace(char* input_str, int* out_num_tokens) {
 /**
  * @brief 使用于本项目的特殊strlen，当str指向NULL时，返回0
  * 
- * @param str 
- * @return int 
+ * @param str 需要获取长度的字符串
+ * @return int 字符串长度
  */
 int hotfix_strlen(char* str) {
     if (str == NULL){
@@ -212,8 +232,8 @@ int hotfix_strlen(char* str) {
 /**
  * @brief 使用于本项目的特殊strcat，当dest或str指向NULL时，不进行操作
  * 
- * @param dest 
- * @param src 
+ * @param dest 目标字符串
+ * @param src 源字符串
  */
 void hotfix_strcat(char* dest, char* src) {
     if (dest == NULL || src == NULL) {
