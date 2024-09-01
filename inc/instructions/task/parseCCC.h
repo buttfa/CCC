2a3f47138d79b07c52bdd9b420fb8c5693deb2b5
buttfa <1662332017@qq.com>
2024-09-01 14:22:01 +0800

    style: 补充了parseCCC.c/.h的文件和函数注释，并注释了已经不再使用的函数parseCCCtask。 / Added file and function comments for parseCCC. c/. h, and commented on the function parseCCCtask that is no longer in use.

--- inc/instructions/task/parseCCC.h
+++ inc/instructions/task/parseCCC.h
@@ -1,11 +1,19 @@
 #ifndef __PARSECCC_H__
 #define __PARSECCC_H__
+/**
+ * @file parseCCC.h
+ * @author  buttfa (1662332017@qq.com)
+ * @brief 解析并获取CCC编译任务所需的变量
+ * @version 0.1
+ * @date 2024-09-01
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <ccc.h>
-#include <task.h>
+
+#include "compileTask.h"
 #include "ini.h"
 
 struct ARG{
@@ -14,19 +22,19 @@ struct ARG{
 };
 
 
-/**
- * @brief 解析ccc文件中的内容填入ccc文件信息
- * 
- * @param ccc_path 
- * @return compileTask 
- */
-struct COMPILE_TASK parseCCCtask(char* ccc_path);
+// /**
+//  * @brief 解析ccc文件中的内容填入ccc文件信息
+//  * 
+//  * @param ccc_path 
+//  * @return compileTask 
+//  */
+// struct COMPILE_TASK parseCCCtask(char* ccc_path);
 
 /**
- * @brief 解析section中的内容填入compileTask
+ * @brief 解析section中的变量填入compileTask
  * 
- * @param ini_ptr 
- * @return struct COMPILE_TASK 
+ * @param task_section_ptr 编译任务的section的指针 
+ * @return struct COMPILE_TASK 解析所得的compileTask编译任务变量
  */
 struct COMPILE_TASK parseCCCIni(section* task_section_ptr);
 
